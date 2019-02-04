3423f2527bceeca013c212687121e530d7eedcf6
Scott Ratigan <scotteratigan@gmail.com>
2019-02-04 09:54:39 -0800

    Added morgan logger in dev mode

--- server.js
+++ server.js
@@ -1,40 +1,53 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose'); //https://mongoosejs.com/docs/index.html
-const cheerio = require('cheerio');
-const axios = require('axios');
+const logger = require("morgan"); // request logger
+// const cheerio = require('cheerio');
+// const axios = require('axios');
 
 const EXPRESS_PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	console.log("Connected to mongoose database.");
-	var kittySchema = new mongoose.Schema({
+	const kittySchema = new mongoose.Schema({
 		name: String
 	});
-	var Kitten = mongoose.model('Kitten', kittySchema);
-	var myKitten = new Kitten({ name: 'Holly' });
+	const Kitten = mongoose.model('Kitten', kittySchema);
+	const myKitten = new Kitten({
+		name: 'Holly'
+	});
 	console.log("My kitten is named:", myKitten.name); // 'Holly'
 	myKitten.save((err, myKitten) => {
+		if (err) {
+			throw new Error(err);
+		}
 		console.log("Saved kitten:", myKitten.name);
-	})
+	});
 });
-
-
 
 const app = express();
 app.engine(
 	"handlebars",
 	exphbs({
-	  defaultLayout: "main"
+		defaultLayout: "main"
 	})
 );
 
 app.set("view engine", "handlebars");
+app.use(logger("dev")); // log http requests on dev server
+
+// Parse request body as JSON:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder:
+app.use(express.static("public"));
 
 // Routes
 // require('./routes/apiRoutes')(app);
